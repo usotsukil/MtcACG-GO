a1fabe2ea8df4ffe3349c9876fc1825112368131
usotsukil <001@tyrlink.dpdns.org>
2025-12-30 22:42:40 +0800

    Update index.js

--- web-worker/src/index.js
+++ web-worker/src/index.js
@@ -1,5 +1,6 @@
 import { htmlHome, htmlAbout } from './templates.js';
 import { proxyTelegramImage, handleDetail, handleApiPosts, handleBgRandom } from './logic.js';
+import { handleArtists } from './logic.js';
 
 export default {
   async fetch(request, env, ctx) {
@@ -63,6 +64,11 @@ export default {
       return new Response(htmlHome(), { 
         headers: { 'Content-Type': 'text/html;charset=UTF-8', 'Cache-Control': 'public, max-age=60'}
       });
+    }
+
+    // 添加路由
+    if (path === '/artists') {
+      return await handleArtists(url, env);
     }
 
     // 7. 默认兜底
